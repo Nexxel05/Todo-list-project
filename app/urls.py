ad96c3002aefc2a7b1e69bed0ecd567af836a791
Kostyantyn Chernyk <chernykko@gmail.com>
2023-02-25 17:28:46 +0000

    Create Tag update, delete and create views and register them in urls.py

--- app/urls.py
+++ app/urls.py
@@ -5,15 +5,22 @@ from app.views import (
     TagListView,
     TaskCreateView,
     TaskDeleteView,
-    TaskUpdateView
+    TaskUpdateView,
+    TagCreateView,
+    TagUpdateView,
+    TagDeleteView,
 )
 
 urlpatterns = [
     path("", index, name="index"),
-    path("tasks/create", TaskCreateView.as_view(), name="task-create"),
-    path("tasks/<int:pk>/delete", TaskDeleteView.as_view(), name="task-delete"),
-    path("tasks/<int:pk>/update", TaskUpdateView.as_view(), name="task-update"),
-    path("tags", TagListView.as_view(), name="tag-list"),
+    path("tasks/create/", TaskCreateView.as_view(), name="task-create"),
+    path("tasks/<int:pk>/delete/", TaskDeleteView.as_view(), name="task-delete"),
+    path("tasks/<int:pk>/update/", TaskUpdateView.as_view(), name="task-update"),
+
+    path("tags/create/", TagCreateView.as_view(), name="tag-create"),
+    path("tags/", TagListView.as_view(), name="tag-list"),
+    path("tags/<int:pk>/update/", TagUpdateView.as_view(), name="tag-update"),
+    path("tags/<int:pk>/delete/", TagDeleteView.as_view(), name="tag-delete"),
 ]
 
 app_name = "app"
--- app/views.py
+++ app/views.py
@@ -33,3 +33,20 @@ class TaskUpdateView(generic.UpdateView):
 
 class TagListView(generic.ListView):
     model = Tag
+
+
+class TagCreateView(generic.CreateView):
+    model = Tag
+    fields = "__all__"
+    success_url = reverse_lazy("app:tag-list")
+
+
+class TagUpdateView(generic.UpdateView):
+    model = Tag
+    fields = "__all__"
+    success_url = reverse_lazy("app:tag-list")
+
+
+class TagDeleteView(generic.DeleteView):
+    model = Tag
+    success_url = reverse_lazy("app:tag-list")

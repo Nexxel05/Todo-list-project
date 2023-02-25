f5395ae7dd0d04c99641bc9b8f082ad87bd7ee6b
Kostyantyn Chernyk <chernykko@gmail.com>
2023-02-25 17:09:33 +0000

    Create Task update, delete and create views and register them in urls.py

--- app/urls.py
+++ app/urls.py
@@ -1,10 +1,19 @@
 from django.urls import path
 
-from app.views import index, TagListView
+from app.views import (
+    index,
+    TagListView,
+    TaskCreateView,
+    TaskDeleteView,
+    TaskUpdateView
+)
 
 urlpatterns = [
     path("", index, name="index"),
-    path("tags", TagListView.as_view(), name="tag-list")
+    path("tasks/create", TaskCreateView.as_view(), name="task-create"),
+    path("tasks/<int:pk>/delete", TaskDeleteView.as_view(), name="task-delete"),
+    path("tasks/<int:pk>/update", TaskUpdateView.as_view(), name="task-update"),
+    path("tags", TagListView.as_view(), name="tag-list"),
 ]
 
 app_name = "app"
--- app/views.py
+++ app/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
 from django.views import generic
 
 from app.models import Task, Tag
@@ -11,6 +12,23 @@ def index(request):
         "tasks": tasks
     }
     return render(request, "app/index.html", context)
+
+
+class TaskCreateView(generic.CreateView):
+    model = Task
+    fields = "__all__"
+    success_url = reverse_lazy("app:index")
+
+
+class TaskDeleteView(generic.DeleteView):
+    model = Task
+    success_url = reverse_lazy("app:index")
+
+
+class TaskUpdateView(generic.UpdateView):
+    model = Task
+    fields = "__all__"
+    success_url = reverse_lazy("app:index")
 
 
 class TagListView(generic.ListView):

fe58546a3a0455e44670aac877ed2a12dcbdfc78
Kostyantyn Chernyk <chernykko@gmail.com>
2023-02-25 16:15:12 +0000

    Include app to settings.py and create Task and Tag models

--- app/models.py
+++ app/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Tag(models.Model):
+    name = models.CharField(max_length=63)
+
+
+class Task(models.Model):
+    content = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    deadline = models.DateTimeField(blank=True, null=True)
+    is_completed = models.BooleanField(default=False)
+    tags = models.ManyToManyField(
+        Tag,
+        on_delete=models.CASCADE,
+        related_name="tasks"
+    )
+
